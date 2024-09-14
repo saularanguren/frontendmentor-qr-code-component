50eb129359652747d2742d2ac49216ebc06dbb08
Saul Gabriel Aranguren <gabomoralejo@gmail.com>
2024-09-14 01:03:39 -0400

    full development

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'slate300': '#D5E1EF',
+				'slate900': '#1F314F',
+				'slate500': '#68778D',
+			},
+			boxShadow: {
+				'card-shadow': '0 25px 25px 0 rgba(0, 0, 0, 0.0477)',
+			},
+			fontFamily: {
+				'outfit': ['"Outfit"', 'system-ui'],
+			},
+		},
 	},
 	plugins: [],
 }
